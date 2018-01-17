298d413a0135692af291454323c95752b5267033
spring-boot <30676507+liu119631@users.noreply.github.com>
2018-01-17 10:38:15 +0800

    Update App.java

--- src/main/java/com/liu/spring_boot_mybatis/App.java
+++ src/main/java/com/liu/spring_boot_mybatis/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.kfit.*")//扫描：该包下相应的class,主要是MyBatis的持久化类.
+@MapperScan("com.liu.*")//扫描：该包下相应的class,主要是MyBatis的持久化类.
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
